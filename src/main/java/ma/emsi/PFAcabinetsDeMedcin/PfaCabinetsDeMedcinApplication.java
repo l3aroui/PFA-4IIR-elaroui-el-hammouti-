00a4abad468a9e12ebb4634922ff0538581a910b
zakaria elaroui <zakariaelaroui122@gmail.com>
2024-04-29 18:26:39 +0100

    push the spring project (empty)

---
+++ src/main/java/ma/emsi/PFAcabinetsDeMedcin/PfaCabinetsDeMedcinApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.PFAcabinetsDeMedcin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfaCabinetsDeMedcinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfaCabinetsDeMedcinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/emsi/PFAcabinetsDeMedcin/PfaCabinetsDeMedcinApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.PFAcabinetsDeMedcin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfaCabinetsDeMedcinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
