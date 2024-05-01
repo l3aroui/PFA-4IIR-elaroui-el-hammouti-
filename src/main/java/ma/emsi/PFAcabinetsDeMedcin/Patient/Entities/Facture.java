5dc1d8fda3b4332c3c6c6a176415ee989f6fd8be
zakaria elaroui <zakariaelaroui122@gmail.com>
2024-05-01 19:43:57 +0100

    creation les classes qui ont relation avec Patient

--- src/main/java/ma/emsi/PFAcabinetsDeMedcin/Patient/Entities/Facture.java
+++ src/main/java/ma/emsi/PFAcabinetsDeMedcin/Patient/Entities/Facture.java
@@ -1,22 +1,25 @@
 package ma.emsi.PFAcabinetsDeMedcin.Patient.Entities;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-import ma.emsi.PFAcabinetsDeMedcin.User.entities.UserApp;
 
+import java.util.Date;
 
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Patient extends UserApp {
+public class Facture {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Float montantTT;
+    private Date dateFact;
+    private Boolean payee;
 }
---
+++ src/main/java/ma/emsi/PFAcabinetsDeMedcin/Patient/Entities/Service.java
@@ -0,0 +1,20 @@
+package ma.emsi.PFAcabinetsDeMedcin.Patient.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor @NoArgsConstructor
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String detail;
+    @OneToMany
+    private List<RendezVous> rendezVous;
+}
