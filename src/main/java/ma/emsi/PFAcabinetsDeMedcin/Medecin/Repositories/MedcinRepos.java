e184246d2972af88119fa6500dd46dc008ed7ab3
zakaria elaroui <zakariaelaroui122@gmail.com>
2024-05-02 21:34:12 +0100

    5/2/2042 drte recherche medcin o drte class addresse nssite madrtche chi l3ibate

--- src/main/java/ma/emsi/PFAcabinetsDeMedcin/Medecin/Repositories/MedcinRepos.java
+++ src/main/java/ma/emsi/PFAcabinetsDeMedcin/Medecin/Repositories/MedcinRepos.java
@@ -2,6 +2,7 @@ package ma.emsi.PFAcabinetsDeMedcin.Medecin.Repositories;
 
 import ma.emsi.PFAcabinetsDeMedcin.Medecin.Entities.Medcin;
 import ma.emsi.PFAcabinetsDeMedcin.Medecin.Entities.Specialite;
+import ma.emsi.PFAcabinetsDeMedcin.User.entities.Addresse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,7 @@ import java.util.List;
 @Repository
 public interface MedcinRepos extends JpaRepository<Medcin,Long> {
      List<Medcin> findByNomAndPrenom(String nom,String prenom);
-     List<Medcin> findByAddresse(String addresse);
+     List<Medcin> findByAddresse(Addresse addresse);
      List<Medcin> findBySpecialite(Specialite specialite);
 
 }
--- src/main/java/ma/emsi/PFAcabinetsDeMedcin/Patient/services/PatientService.java
+++ src/main/java/ma/emsi/PFAcabinetsDeMedcin/Patient/services/PatientService.java
@@ -4,6 +4,7 @@ package ma.emsi.PFAcabinetsDeMedcin.Patient.services;
 import ma.emsi.PFAcabinetsDeMedcin.Medecin.Entities.Medcin;
 import ma.emsi.PFAcabinetsDeMedcin.Medecin.Entities.Specialite;
 import ma.emsi.PFAcabinetsDeMedcin.Medecin.Repositories.MedcinRepos;
+import ma.emsi.PFAcabinetsDeMedcin.User.entities.Addresse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class PatientService {
     public List<Medcin> afficherLesMedcin(){
         return medcinRepos.findAll();
     }
-    public List<Medcin> afficherParAddresse(String addresse){
+    public List<Medcin> afficherParAddresse(Addresse addresse){
         return medcinRepos.findByAddresse(addresse);
     }
 
