c65acd3f115c84bde5e49302bb35bd08215010fe
zakaria elaroui <zakariaelaroui122@gmail.com>
2024-05-05 09:16:39 +0100

    5/5/2024 je pense que j'ai termine RECHERCHER MEDCIN pour patient non tester

--- src/main/java/ma/emsi/PFAcabinetsDeMedcin/User/repositories/RoleRepo.java
+++ src/main/java/ma/emsi/PFAcabinetsDeMedcin/User/repositories/RoleRepo.java
@@ -1,4 +1,9 @@
 package ma.emsi.PFAcabinetsDeMedcin.User.repositories;
 
-public interface RoleRepo extends {
+import ma.emsi.PFAcabinetsDeMedcin.User.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepo extends JpaRepository<Role,Long> {
 }
